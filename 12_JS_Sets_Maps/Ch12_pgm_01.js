a1f063e02773ca42e9e09702240567745aabde32
Sandhya4931 <sandhya4931m@gmail.com>
2024-07-13 06:10:58 +0530

    JS-Setmaps

--- 12_JS_Sets_Maps/Ch12_pgm_01.js
+++ 12_JS_Sets_Maps/Ch12_pgm_01.js
@@ -1,6 +1,29 @@
-const a = [4, 5, 8, 9]
-const b = [3, 4, 5, 7]
-const countries = ['Finland', 'Sweden', 'Norway']
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7];
+const countries = ['Finland', 'Sweden', 'Norway'];
+
+const emptySet = new Set();
+
+const set0to10 = new Set();
+for (let i = 0; i <= 10; i++) {
+    set0to10.add(i);
+}
+set0to10.delete(5);
+
+emptySet.clear();
+
+const countrySet = new Set(countries);
+
+const countryLengthMap = new Map();
+countries.forEach(country => {
+    countryLengthMap.set(country, country.length);
+});
+
+console.log("Empty Set:", emptySet);
+console.log("Set 0 to 10:", set0to10);
+console.log("Country Set:", countrySet);
+console.log("Country Length Map:", countryLengthMap);
+
 
 
 // create an empty set
