7d8476d680c48aab7fc9f044fddfdeb33c956925
Sandhya-4931 <sandhya4931m@gmail.com>
2024-07-31 22:02:30 +0530

    JS-closure

---
+++ 16_JS_Closures/Ch16_pgm_01.js
@@ -0,0 +1,34 @@
+// Level 1
+
+// Create a closure which has one inner function
+
+// Level 2
+
+// Create a closure which has three inner functions
+
+// Level 3
+
+// Create a personAccount out function. It has firstname, lastname, incomes, expenses inner 
+// variables. It has totalIncome, totalExpense, accountInfo,addIncome, addExpense and 
+// accountBalance inner functions. Incomes is a set of incomes and its description and 
+// expenses is also a set of expenses and its description.
+function outerFunction() {
+  let count = 0;
+  function plusOne() {
+      count++
+      return count
+  }
+  function minusOne() {
+      count--
+      return count
+  }
+
+  return {
+      plusOne:plusOne(),
+      minusOne:minusOne()
+  }
+}
+const innerFuncs = outerFunction()
+
+console.log(innerFuncs.plusOne)
+console.log(innerFuncs.minusOne)
