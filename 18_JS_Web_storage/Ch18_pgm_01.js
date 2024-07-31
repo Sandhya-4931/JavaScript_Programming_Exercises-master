9153b7c677637ec6dfe7d816939c4f64f9bab019
Sandhya-4931 <sandhya4931m@gmail.com>
2024-07-31 22:03:57 +0530

    JS-web storage

---
+++ 18_JS_Web_storage/Ch18_pgm_01.js
@@ -0,0 +1,22 @@
+// Level 1
+
+// Store you first name, last name, age, country, city in your browser localStorage.
+
+
+// Level 2
+
+// Create a student object. The student object will have first name, last name, age, skills, 
+// country, enrolled keys and values for the keys. Store the student object in your browser 
+// localStorage.
+
+
+// Level 3
+
+// Create an object called personAccount. It has firstname, lastname, incomes, expenses 
+// properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and 
+// accountBalance methods. Incomes is a set of incomes and its description and expenses is 
+// also a set of expenses and its description.
+// HTML web storage provides two objects for storing data on the client:
+
+// - window.localStorage - stores data with no expiration date
+// - window.sessionStorage - stores data for one session (data is lost when the browser tab is closed)Most modern browsers support Web Storage, however it is good to check browser support for localStorage and sessionStorage. Let us see the available methods for the Web Storage objects.
